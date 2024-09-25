87ec9f5fea31b3d585835dcce4f28a30eee91fc6
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-09-25 17:16:10 +0200

    [UPD] created footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,52 @@
+<footer class="bg-dark text-white py-5">
+
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-3 border border-2 border-primary border-top-0 border-bottom-0 px-4">
+                <H3>Laravel PRODUCTION</H3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea dolor aliquam labore, veritatis et
+                    dignissimos provident inventore minus eligendi. Fuga magnam ex illo laboriosam rem blanditiis sint
+                    cumque nostrum placeat!</p>
+            </div>
+            <div class="col-3 border border-2 border-primary border-top-0 border-bottom-0">
+                <h3>CONTACTS LIST</h3>
+                <ul class="list-unstyled lh-lg">
+                    <li><span class="text-primary">PHONE :</span> +123 45 555</li>
+                    <li><span class="text-primary">HEAD OFFICE :</span> <span class="fst-italic">Cliff Lane, 854, Alaska</span></li>
+                    <li><span class="text-primary">REGISTERED OFFICE :</span> <span class="fst-italic">Clove Street, 1210, Washington</span></li>
+                    <li><span class="text-primary">EMAIL :</span> laravel.production@domain.com</li>
+                </ul>
+            </div>
+            <div class="col-3 border border-2 border-primary border-top-0 border-bottom-0">
+                <h3>SOCIAL MEDIA</h3>
+                <ul class="list-unstyled">
+                    <li class="mb-3 d-flex justify-content-center align-items-center">
+                        <a href="#"><i class="bi bi-instagram fs-3"></i></a>
+                        <span class="fst-italic fw-semibold ms-2">Instagram Page</span>
+                    </li>
+                    <li class="mb-3 d-flex justify-content-center align-items-center">
+                        <a href="#"><i class="bi bi-facebook fs-3"></i></a>
+                        <span class="fst-italic fw-semibold ms-2">Facebook Page</span>
+                    </li>
+                    <li class="mb-3 d-flex justify-content-center align-items-center">
+                        <a href="#"><i class="bi bi-twitter fs-3"></i></a>
+                        <span class="fst-italic fw-semibold ms-2">Twitter Page</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-3 border border-2 border-primary border-top-0 border-bottom-0">
+                <h3>NEWSLETTER</h3>
+                <form>
+                    <div class="form-group my-4">
+                        <input type="email" class="form-control" id="email" placeholder="Enter your email">
+                    </div>
+                    <div class="text-end">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+</footer>
